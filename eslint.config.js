8f719a7ea963861d95eae2fb2b75bf2759f9d380
Nate Berkopec <nate.berkopec@gmail.com>
2025-03-12 07:43:15 +0900

    Bolt upgrade

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import js from '@eslint/js';
-import globals from 'globals';
+const js = require('@eslint/js');
+const globals = require('globals');
 
-export default [
+module.exports = [
   js.configs.recommended,
   {
+    files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
